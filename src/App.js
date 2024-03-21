42266fcb534a62a5b0c0eadbc30130ff68805b1c
Богдан Козороз <sir.bodik2011@outlook.com>
2024-03-21 12:45:40 +0200

    small changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header'
-  class App extends React.Component{
-    myOnClick = () => alert("clicked");
-    render(){
-    return (<div><Header title="Шапка сайта"/>
-      <h1 onClick = {this.myOnClick}>Hello world</h1>
+class App extends React.Component {
+  HelloText = "Hello world";
+  myOnClick = () => alert("clicked");
+  render() {
+    return (<div><Header title="Шапка сайта" />
+      <h1 onClick={this.myOnClick}>{this.HelloText}</h1>
       <h2>Hello react</h2>
-      </div>)
-    }
+    </div>)
   }
+}
 
 export default App
